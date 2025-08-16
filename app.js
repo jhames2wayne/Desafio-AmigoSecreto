a2ec0822e2d43a7acf0fff21b49409befd5981aa
jhames2wayne <jhames.wayne@live.com>
2025-08-16 17:53:41 -0300

    Implementado funcão para adicionar os amigos.

--- app.js
+++ app.js
@@ -1,2 +1,28 @@
-//Criado array para adicionar nome dos amigos.
+//?Criado array para adicionar nome dos amigos.
 let amigos = [];
+/**
+ * ?Função para adicionar os nome a lista de amigos.
+ * Ela é ativada quando o usuário clica no botão Adicionar.
+ */
+function adicionarAmigo() {
+  // Pega o elemento do site onde o usuário digita o nome.
+  let inputAmigo = document.getElementById("amigo");
+  // Pega o valor digitado e remove espaços no inicio e no fim.
+  let nomeAmigo = inputAmigo.value.trim();
+
+  // validar se o campo nome esta vazio.
+  // !Se o campo estiver vazio, exibe um alerta e para a função.
+  if (nomeAmigo === "") {
+    alert("Precisa de um nome para adicionar um amigo.");
+    // Para a função caso o nome seja inválido.
+    return;
+  }
+
+  // Adiciona o nome digitado ao array amigos.
+  amigos.push(nomeAmigo);
+
+  // Limpa o campo de texto para o próximo nome.
+  inputAmigo.value = "";
+  // Deixa o cursor no campo, pronto para a próxima digitação.
+  inputAmigo.focus();
+}
