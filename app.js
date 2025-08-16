79aa376d10b51af661e22bf4c7f06d3cd09373af
jhames2wayne <jhames.wayne@live.com>
2025-08-16 18:59:04 -0300

    Corrigido linha do codigo  que nao mostrava os nomes adicionados.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-//?Criado array para adicionar nome dos amigos.
+// Criado array para adicionar nome dos amigos.
 let amigos = [];
 /**
- * ?Função para adicionar os nome a lista de amigos.
+ * Função para adicionar os nome a lista de amigos.
  * Ela é ativada quando o usuário clica no botão Adicionar.
  */
 function adicionarAmigo() {
@@ -10,8 +10,8 @@ function adicionarAmigo() {
   // Pega o valor digitado e remove espaços no inicio e no fim.
   let nomeAmigo = inputAmigo.value.trim();
 
-  // validar se o campo nome esta vazio.
-  // !Se o campo estiver vazio, exibe um alerta e para a função.
+  // Valida se o campo nome esta vazio.
+  // Se o campo estiver vazio, exibe um alerta e para a função.
   if (nomeAmigo === "") {
     alert("Precisa de um nome para adicionar um amigo.");
     // Para a função caso o nome seja inválido.
@@ -21,13 +21,16 @@ function adicionarAmigo() {
   // Adiciona o nome digitado ao array amigos.
   amigos.push(nomeAmigo);
 
+  // Alualiza a lista toda vez que um novo nome é adicionado.
+  atualizarLista();
+
   // Limpa o campo de texto para o próximo nome.
   inputAmigo.value = "";
   // Deixa o cursor no campo, pronto para a próxima digitação.
   inputAmigo.focus();
 }
 /**
- * ?Função para atualizar a exibição da lista de amigos na página.
+ * Função para atualizar a exibição da lista de amigos na página.
  * Ela é chamada após inserir um novo nome.
  */
 function atualizarLista() {
@@ -37,14 +40,14 @@ function atualizarLista() {
   lista.textContent = amigos.join(", ");
 }
 /**
- * ?Função para sortear um nome da lista de amigos.
+ * Função para sortear um nome da lista de amigos.
  * Ela é ativada pelo botão Sortear amigo.
  */
 function sortearAmigo() {
   // Pega o elemento onde o resultado do sorteio será exibido.
   let resultado = document.getElementById("resultado");
 
-  // !valida se não houver nomes na lista, não é possível sortear.
+  // Valida se não houver nomes na lista, não é possível sortear.
   if (amigos.length < 1) {
     alert("Adicione pelo menos um amigo antes de sortear!");
     return;
