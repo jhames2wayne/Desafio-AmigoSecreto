1dfa131da1b8b6e376dd3286f1d56181b2aaa24c
jhames2wayne <jhames.wayne@live.com>
2025-08-16 17:40:32 -0300

    Criado array para adicionar nome dos amigos

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//Criado array para adicionar nome dos amigos.
+let amigos = [];
